47d7d49a663f806d658c4b267fcb5fc60d5a3759
gro97 <gro.oleivsgard@gmail.com>
2021-09-11 12:49:22 +0200

    Added instances in MidlertidigMainTest() to check if output is correct before testing. Added thourough descriptions in maks() and ombyttinger(), but ombyttinger() is still not working properly. Created rotasjon(), it is working properly but the method needs to check the length of array and not throw exception if length < 2.

--- src/main/java/no/oslomet/cs/algdat/Oblig1/MidlertidigMainTest.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/MidlertidigMainTest.java
@@ -3,13 +3,17 @@ package no.oslomet.cs.algdat.Oblig1;
 // Midlertidig main-metode for å sjekke at metodene skriver ut ønsket resultat.
 
 import static no.oslomet.cs.algdat.Oblig1.Oblig1.antallUlikeSortert;
+import static no.oslomet.cs.algdat.Oblig1.Oblig1.rotasjon;
 
 public class MidlertidigMainTest {
 
     public static void main(String[] args) {
-        int [] tabellOppg2 = {3, 3, 4, 5, 5, 6, 7, 7, 7, 8};
+        //int [] tabellOppg2 = {3, 3, 4, 5, 5, 6, 7, 7, 7, 8};
 
-        int ulikeTall = antallUlikeSortert(tabellOppg2);
-        System.out.println("Det er " + ulikeTall + " ulike tall i tabellen.");
+        //int ulikeTall = antallUlikeSortert(tabellOppg2);
+        //System.out.println("Det er " + ulikeTall + " ulike tall i tabellen.");
+
+        char [] a = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        rotasjon(a);
     }
 }
--- src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -13,14 +13,14 @@ public class Oblig1 {
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
 
-        //må lages
-        //throw new UnsupportedOperationException();
         if(a.length <= 0){
             throw new NoSuchElementException();
         }
-        //Bruker to for-løkker
+        //Bruker to for-løkker, som vist i forelesning for Bubble Sort 07.09.2021
 
-        // (1) Ytre løkke sorterer ett tall per gjennomgang
+        // (1) Ytre løkke sorterer ett tall per gjennomgang, starter på indeks 0 slik at tabellen
+        // blir sortert fra venstre til høyre.
+
         for(int i = 0; i < a.length-1; i++){
             // (2) Indre løkke bytter om verdiene på posisjonene
             for(int j = 0; j < a.length-1; j++) {
@@ -41,29 +41,38 @@ public class Oblig1 {
 
 
     public static int ombyttinger(int[] a) {
-        //throw new UnsupportedOperationException();
+
+        //tar inn et array som allerede er sortert - hvordan få inn et usortert int [] a?
+
+        /**
+         * Det blir flest ombyttinger dersom en permutasjon av tallene 1 til N er synkende fra venstre til høyre
+         * Dermed vil det bli en ombytting for hver økning av indeksen.
+         *
+         * Det blir færrest ombyttinger dersom en permutasjon av tallene fra 1 til N er stigende fra venstre til høyre,
+         * fordi tallene allerede vil være sortert og det blir dermed ikke behov for mange ombyttinger.
+         *
+         * I gjennomsnitt vil det være H(n) = log(n) + 0,577 ombyttinger, hvor n = a.length.
+         *
+         */
 
         int teller = 0;
 
-        for(int i = 0; i < a.length-1; i++){
+        //regn ut gjennomsnitt av ombyttinger i UnitTest
+        // int gjennomsnitt = teller / a.length;
 
-            // (2) Indre løkke bytter om verdiene på posisjonene
+        // Bruker liknende for-løkke som i maks(), med ytre og indre løkke.
+        for(int i = 0; i < a.length-1; i++){
             for(int j = 0; j < a.length-1; j++) {
 
-                // (3) Dersom det er en ombytting økes teller
+                // Dersom en ombytting blir utført, økes teller.
                 if (a[j] > a[j + 1]) {
-                    //System.out.println("Inversj2on! Bytter plass på " + j + " og " + (j-1) );
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
                     teller++;
-
                 }
-
             }
-
         } return teller;
-
     }
 
     ///// Oppgave 2 //////////////////////////////////////
@@ -106,7 +115,31 @@ public class Oblig1 {
 
     ///// Oppgave 5 //////////////////////////////////////
     public static void rotasjon(char[] a) {
-        throw new UnsupportedOperationException();
+        /**
+         * Lag metoden
+         * public static void rotasjon(char[] a). Den skal «rotere» innholdet i tabellen a én enhet.
+         * En rotasjon i en tom tabell eller i en tabell med nøyaktig ett element er ingen feilsituasjon. Men
+         * rotasjonen vil da ikke endre noe.
+         */
+
+        //Bruker en liknende rotasjonsmetode 1.3.13 d) som beskrevet i kompendiet i avsnitt 1.3.13,
+        // "Forskyvninger og rotasjoner".
+
+        // (1) Oppretter en hjelpetabell
+        char [] b = Arrays.copyOfRange(a, a.length - 1, a.length);
+
+        // (2) Oppretter variabel for antall plasser tabellen skal forsvyes, i dette
+        // tilfelle 1.
+        int d = 1;
+
+        //(3) For-løkke forskyver, fra venstre, alle verdiene en plass mot høyre.
+        for (int i = a.length - d; i >= d; i--){
+            a[i] = a[i - d];
+        }
+
+        // (4) Kopierer verdi fra indeks 0 i [] b til indeks 1 i [] a.
+        System.arraycopy(b, 0, a, 0, d);
+        System.out.println("Array etter 1 flytt" + Arrays.toString(a));
     }
 
     ///// Oppgave 6 //////////////////////////////////////
