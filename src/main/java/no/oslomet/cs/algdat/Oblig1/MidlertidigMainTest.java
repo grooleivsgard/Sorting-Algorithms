a7372c82800ab3315f81821cf50f46a8fd3d924c
ennyli <69962472+ennyli@users.noreply.github.com>
2021-09-12 19:43:41 +0200

    Startet på oppg3
    - Startet på oppgave 3.

--- src/main/java/no/oslomet/cs/algdat/Oblig1/MidlertidigMainTest.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/MidlertidigMainTest.java
@@ -2,18 +2,25 @@ package no.oslomet.cs.algdat.Oblig1;
 
 // Midlertidig main-metode for å sjekke at metodene skriver ut ønsket resultat.
 
-import static no.oslomet.cs.algdat.Oblig1.Oblig1.antallUlikeSortert;
-import static no.oslomet.cs.algdat.Oblig1.Oblig1.rotasjon;
+import static no.oslomet.cs.algdat.Oblig1.Oblig1.*;
 
 public class MidlertidigMainTest {
 
     public static void main(String[] args) {
-        int [] tabellOppg2 = {3, 3, 4, 5, 5, 6, 7, 7, 7, 8};
+        // Oppgave 2 og 3
+        int [] tabellOppg2 = {5, 3, 7, 4, 3, 5, 7, 8, 6, 7};
 
         int ulikeTall = antallUlikeSortert(tabellOppg2);
         System.out.println("Det er " + ulikeTall + " ulike tall i tabellen.");
 
         char [] a = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
         rotasjon(a);
+
+        // Oppgave 7
+        String a = flett("ABC","DEFGH");
+        String b = flett("IJKLMN","OPQ");
+        String b = flett("","AB");
+        System.out.println(a + " " + b + " " + c);
+// Utskrift: ADBECFGH IOJPKQLMN AB
     }
 }
--- src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -105,7 +105,19 @@ public class Oblig1 {
 
     ///// Oppgave 3 //////////////////////////////////////
     public static int antallUlikeUsortert(int[] a) {
-        throw new UnsupportedOperationException();
+
+        // Oppretter en teller for å telle antall ulike tall.
+        int teller = 1;
+
+        // Lager en for-løkke som går igjennom tabellen.
+        for (int i = 0; i < a.length -1; ++i) {
+            // Hvis tabellen er tom vil teller returnere 0.
+            if (a.length == 0) {
+                teller = 0;
+            }
+            // KODE MANGLER HER FOR Å FULLFØRE TELLINGEN.
+        }
+        return teller;
     }
 
     ///// Oppgave 4 //////////////////////////////////////
